b1ba2c52832dc93e16bf2feaea058414a8f2d5c7
kaiser <341757594@qq.com>
2022-02-17 15:06:27 +0800

    webpack 添加 externals

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,6 @@
-import * as React from 'react';
-import ReactDom from 'react-dom'
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import Button from './button'
 
+ReactDOM.render(<Button></Button>, document.body)
 console.log(React)
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,44 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	mode:'production',
-	entry:{
-		myUi:'./lib/index.tsx'
-	},
-	output:{
-		path:path.resolve(__dirname,'dist/lib'),
-		library:'myUi',
-		libraryTarget:'umd'
-	},
-	module:{
-		rules:[
-			{
-				test:/\.tsx?$/,
-				loader:'awesome-typescript-loader'
-			}
-		]
-	},
-	plugins:[
-		new HtmlWebpackPlugin({
-			title:'myUi',
-			template:'index.html'
-		})
-	]
+  mode: 'production',
+  entry: {
+    myUi: './lib/index.tsx',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'myUi',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'myUi',
+      template: 'index.html',
+    }),
+  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+  },
 }
